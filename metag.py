31865b32ca7488e18e6ca7a19c3816434328f6ce
Jose Manuel Garcia Ramirez <64587697+Sannin-Mode@users.noreply.github.com>
2023-04-24 10:26:03 +0200

    Create metag.py

---
+++ metag.py
@@ -0,0 +1,32 @@
+"""
+Programa que toma como parámetros dos secuencias con formato SRRxxx_R1.fastq y SRRxxx_R2.fastq
+Ejecuta consecutivamente Trim Galore!! y Metaphlan4 para conseguir el análisis metagenómico 
+"""
+import os
+import sys
+from os import remove
+
+#Añadimos los archivos declarados como argumentos a las variables
+fw_rds = sys.argv[1] #El archivo R1
+rv_rds = sys.argv[2] #El archivo R2
+
+#Extraemos el nombre de la secuencia para utilizarla en las salidas y lo formateamos
+
+nombre_extracted = os.path.splitext(fw_rds)[0]
+nombre = nombre_extracted[:-3]
+
+
+#Trim Galore!
+
+#os.system("trim_galore -q 20 -j 8 --paired {} {}".format(fw_rds, rv_rds))
+
+#Cambiar nombres de los archivos generados por Trim Galore
+
+trimmed_forward = nombre + "_val_1.fq"
+trimmed_reverse = nombre + "_val_2.fq"
+
+
+#Ejecución de Metaphlan (v4) en dos pasos para asegurar su correcto funcionamiento
+
+os.system("metaphlan {} {} --bowtie2out {}.bowtie2.bz2 --nproc 8 --input_type fastq --unclassified_estimation > {}_metagenome.txt".format(trimmed_forward,trimmed_reverse,nombre,nombre))
+os.system("metaphlan {}.bowtie2.bz --input_type bowtie2out --nproc 8 --unclassified_estimation > {}.txt".format(nombre,nombre))
